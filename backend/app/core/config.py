70bce0d6b009148c27bc636395fd02d4bceefca9
Shubham Nimje <shubhamsnimje@gmail.com>
2026-01-28 20:45:22 +0530

    frontend url added

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    CORS_ORIGINS: List[str] = ["http://localhost:3000", "https://vercel.app"]
+    CORS_ORIGINS: List[str] = ["http://localhost:3000", "https://cmd-assignment-production.netlify.app/campaigns"]
     SECRET_KEY: str = "9f2d8a7c1e4b9a1f7c3d0a6b8e2f4c9a"
     
     class Config:
